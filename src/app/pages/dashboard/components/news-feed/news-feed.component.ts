35cb1235aa6bc6c524290c99b6c537d59cf30622
Manisha Gaonkar <manishagaonkar7875@gmail.com>
2021-07-26 10:15:54 +0530

    Updated the dashboard functionality

--- src/app/pages/dashboard/components/news-feed/news-feed.component.ts
+++ src/app/pages/dashboard/components/news-feed/news-feed.component.ts
@@ -1,77 +1,85 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
-import {
-  of ,
-  Subscription
-} from 'rxjs';
-import {colors} from '../../../../consts';
+import { of, Subscription } from 'rxjs';
+import { colors } from '../../../../consts';
 
 @Component({
   selector: 'app-news-feed',
   templateUrl: './news-feed.component.html',
-  styleUrls: ['./news-feed.component.scss']
+  styleUrls: ['./news-feed.component.scss'],
 })
 export class NewsFeedComponent implements OnInit {
-  constructor(
-    private http: HttpClient,
-    private route: ActivatedRoute,
-){}
+  constructor(private http: HttpClient, private route: ActivatedRoute) {}
   public colors: typeof colors = colors;
-  selectedOption: String = 'topstories'
-  options: any[] = [{label:'Top', value:'topstories'},{label:'Ask', value:'askstories'},{label:'Show', value:'showstories'},{label:'Jobs', value:'jobstories'}]
+  selectedOption: String = 'topstories';
+  options: any[] = [
+    { label: 'Top', value: 'topstories' },
+    { label: 'Ask', value: 'askstories' },
+    { label: 'Show', value: 'showstories' },
+    { label: 'Jobs', value: 'jobstories' },
+  ];
   feed: any;
   feed1: any;
   feed$: Subscription;
+  interval: any;
   pageSize: number = 7;
   offset: number = 0;
   dataSource: any;
-  search:'';
-  searchText
-  ngOnInit(){
-    this.selectedOption = 'topstories'
+  search: '';
+  searchText;
+  ngOnInit() {
+    this.selectedOption = 'topstories';
     this.getHackerNewsData('topstories');
-  //   this.interval = setInterval(() => { 
-  //     this.getData(); 
-  // }, 10000);
+    this.interval = setInterval(() => {
+      this.getData();
+    }, 60000);
   }
 
   getHackerNewsData(val) {
-    this.http.get('https://hacker-news.firebaseio.com/v0/'+val+'.json?print=pretty')
-    .subscribe(data => {
-      this.feed = data
-      this.getData()
-    })
+    this.http
+      .get(
+        'https://hacker-news.firebaseio.com/v0/' + val + '.json?print=pretty'
+      )
+      .subscribe((data) => {
+        this.feed = data;
+        this.getData();
+      });
   }
 
   async getData() {
     let newDetails = [];
     let count = 0;
-    this.feed.slice(this.offset,(this.pageSize + this.offset)).forEach(async id => {
-      console.log(id)
-      this.http.get('https://hacker-news.firebaseio.com/v0/item/'+ id +'.json?print=pretty')
-      .subscribe(res => {
-        console.log(res)
-      this.feed1 = res
-      newDetails.push(this.feed1);
-      console.log(newDetails)
-      count++;
-      if(count === this.pageSize){
-          
-          this.dataSource = newDetails
-          this.offset = this.pageSize + this.offset
-      }
-      })
-      
-    })
+    this.feed
+      .slice(this.offset, this.pageSize + this.offset)
+      .forEach(async (id) => {
+        this.http
+          .get(
+            'https://hacker-news.firebaseio.com/v0/item/' +
+              id +
+              '.json?print=pretty'
+          )
+          .subscribe((res) => {
+            this.feed1 = res;
+            newDetails.push(this.feed1);
+            count++;
+            if (count === this.pageSize) {
+              this.dataSource = newDetails;
+              this.offset = this.pageSize + this.offset;
+            }
+          });
+      });
   }
 
   newsDetails(url) {
     window.open(url);
   }
 
-  authorDetails(author){
-    window.open("https://hacker-news.firebaseio.com/v0/user/"+author+".json?print=pretty");
-    
+  authorDetails(author) {
+    window.open(
+      'https://hacker-news.firebaseio.com/v0/user/' +
+        author +
+        '.json?print=pretty'
+    );
   }
 }
--- src/app/pages/dashboard/components/users/users.component.ts
+++ src/app/pages/dashboard/components/users/users.component.ts
@@ -1,44 +1,37 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
 
-import {
-  Subscription
-} from 'rxjs';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'user-details',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
 })
 export class UserDetailComponent implements OnInit {
-  constructor(
-    private http: HttpClient,
-    private route: ActivatedRoute
-){}
+  constructor(private http: HttpClient, private route: ActivatedRoute) {}
   feed: any;
   feed1: any;
   feed$: Subscription;
   interval: any;
   user: any;
-  id:String;
-  ngOnInit(){
+  id: String;
+  ngOnInit() {
     this.id = this.route.snapshot.paramMap.get('id');
     this.getHackerNewsData(this.id);
-  //   this.interval = setInterval(() => { 
-  //     this.getHackerNewsData(); 
-  // }, 10000);
   }
 
   async getHackerNewsData(id) {
-    let newDetails = [];
-    let count = 0;
-    let postsList = null;
-    this.http.get('https://hacker-news.firebaseio.com/v0/user/'+id+'.json?print=pretty')
-    .subscribe(data => {
-      this.feed = data
-      this.user = this.feed
-    })
+    this.http
+      .get(
+        'https://hacker-news.firebaseio.com/v0/user/' +
+          id +
+          '.json?print=pretty'
+      )
+      .subscribe((data) => {
+        this.feed = data;
+        this.user = this.feed;
+      });
   }
-
 }
