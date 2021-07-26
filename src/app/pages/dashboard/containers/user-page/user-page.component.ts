7b9ac171599dfd84953f7bd52a48d466d22b2f2a
Manisha Gaonkar <manishagaonkar7875@gmail.com>
2021-07-26 09:58:47 +0530

    Added a user details component

---
+++ src/app/pages/dashboard/containers/user-page/user-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'user-page',
+  templateUrl: './user-page.component.html',
+  styleUrls: ['./user-page.component.scss']
+})
+export class UserPageComponent {
+
+  constructor() {
+  }
+}
